07fd8d870a1e728ed12bbbde07b25ca09f53f423
tiandaoyi <495060071@qq.com>
2020-12-06 16:01:47 +0800

    feat(upgrade): add other form options

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,32 +5,30 @@ import EleTooltipInputNum from './TooltipInputNum';
 import EleTooltipRadio from './TooltipRadio';
 import EleTooltipSwitch from './TooltipSwitch';
 
-// EleTooltipInput.install = function (Vue) {
-//   Vue.component(EleTooltipInput.name, EleTooltipInput);
-// }
-
-// EleTooltipSelect.install = function (Vue) {
-//   Vue.component(EleTooltipSelect.name, EleTooltipSelect);
-// }
-
 const installer = {
 	EleTooltipInput,
 	EleTooltipSelect,
+	EleTooltipDate,
+	EleTooltipInputNum,
+	EleTooltipRadio,
+	EleTooltipSwitch
 }
+
 const batchBindInstall = (obj) => {
 	for (let i in obj) {
-		console.log(i)
 		const currInstaller = installer[i]
 		currInstaller.install = (Vue) => {
 			Vue.component(currInstaller.name, currInstaller)
 		}
-		// installer[i].component(installer[i].name, installer[i])
 	}
 }
 batchBindInstall(installer)
 
-
 export {
   EleTooltipInput,
-  EleTooltipSelect
+	EleTooltipSelect,
+	EleTooltipDate,
+	EleTooltipInputNum,
+	EleTooltipRadio,
+	EleTooltipSwitch
 }
